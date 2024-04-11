011ce6674893782f32ee38a0433f19ac7e01aae2
SMToad <bonina9179@gmail.com>
2024-04-11 22:56:38 +0300

    Added the base of the shuffled questions and the ending screen.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 import QUESTIONS from "../questions.js";
+import quizCompleteImage from "../assets/quiz-complete.png";
 
 export default function Quiz(){
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const activeQuestion = QUESTIONS[activeQuestionIndex];
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevAnswers) => {
@@ -13,12 +16,24 @@ export default function Quiz(){
         });
     }
 
+    if(quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImage} alt="Trophy icon"/>
+                <h2>Quiz completed</h2>
+            </div>
+        );
+    }
+    
+    const shuffledAnswers = [...activeQuestion.answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="question">
-                <p>{QUESTIONS[activeQuestionIndex].text}</p>
+                <p>{activeQuestion.text}</p>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                    {shuffledAnswers.map(answer => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>
                                 {answer}
